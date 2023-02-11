ecc6e91c73013bc6a8833e74b8440dcc6c66464b
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-11 17:33:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   const students = studentArray.map(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   studentArray.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+    const newStudent = {id:4, name:"susan", age:"20", marks:45};
+  studentArray.push(newStudent);
+  console.log(newStudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+   const filteredArray = studentArray.filter(student => student.marks >= 50);
+  console.log(filteredArray);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+    const secondArray = [
+    {id:4, name:"lisa", age:"20", marks:70},
+    {id:5, name:"mike", age:"19", marks:65},
+    {id:6, name:"jane", age:"18", marks:55}
+  ];
+  const combinedArray = studentArray.concat(secondArray);
+  console.log(combinedArray);
 }
